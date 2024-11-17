5f0287fc2b9c21bda1805e1bfe04a47d9443dbb1
Ersen Gültepe <ersengultepe@gmail.com>
2024-11-17 15:30:13 +0300

    Initial Commit

---
+++ SwiftUITextAdding/SwiftUITextAddingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextAddingApp.swift
+//  SwiftUITextAdding
+//
+//  Created by Ersen Gültepe on 17.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextAddingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUITextAddingTests/SwiftUITextAddingTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUITextAddingTests.swift
+//  SwiftUITextAddingTests
+//
+//  Created by Ersen Gültepe on 17.11.2024.
+//
+
+import Testing
+
+struct SwiftUITextAddingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
